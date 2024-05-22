f5a01b8194da43e54ea4fd2c3a4436308c6c89b8
Peculiar Richard <pecullozie@gmail.com>
2024-05-22 12:03:14 +0100

    chore: remove secret keys

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,17 +1,22 @@
-
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
+import {
+  getAuth,
+} from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA_glD8xGWesm_YXUAy3T1mUp0HrydEXSo",
-  authDomain: "gwam-cffce.firebaseapp.com",
-  projectId: "gwam-cffce",
-  storageBucket: "gwam-cffce.appspot.com",
-  messagingSenderId: "16272926467",
-  appId: "1:16272926467:web:cd6cfe71eff1f277196b36",
-  measurementId: "G-4TR9GFNK85"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+
+// const analytics = getAnalytics(app);
